69aec7e95f2c5f90b23b14004e0b930675cfdcf0
wade <756059388@qq.com>
2020-08-02 12:04:04 +0800

    广播消息

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ wss.on('connection', function connection(ws) {
   console.log('on cline is connection');
   ws.on('message', function (msg) {
     console.log('msg', msg);
+    // ws.send(msg);
+    //广播消息
+    wss.clients.forEach(clint => {
+      if (ws !== clint && clint.readyState === Websocket.OPEN) clint.send(msg);
+    });
   });
-  ws.send('Message from server');
+  // ws.send('Message from server');
 });
